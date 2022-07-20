1ba8a72176492beeff52280a20c5c69623d23a89
Rodrigo Neves <rnevesdemello@gmail.com>
2022-07-20 20:09:36 -0300

    adição dos exercicios ate o 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio 1.js
@@ -0,0 +1,36 @@
+// adição
+const elementoA = 5;
+const elementoB = 2;
+
+console.log(elementoA+elementoB);
+
+const a = 10;
+const b = 2;
+
+console.log(a-b);
+
+// multiplicação
+
+const am = 10;
+const bm = 2;
+
+console.log(a*b);
+
+// divisão
+
+const ad = 10;
+const bd = 2;
+
+console.log(a/b);
+
+// Módulo
+
+const amol = 12;
+const bmol = 5;                        
+
+console.log(a%b);
+
+
+
+
+
