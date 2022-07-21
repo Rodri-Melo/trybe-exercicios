a2c82d828c1e582361c715c0192979619a2db516
Rodrigo Neves <rnevesdemello@gmail.com>
2022-07-21 19:52:04 -0300

    exs

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio4.js
@@ -0,0 +1,19 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media = 0;
+
+
+    for (index = 0; index < numbers.length; index +=1) {
+        soma += numbers[index];
+}
+
+    media = soma/10;    
+
+    
+if (media > 20) {
+        console.log("valor maior do que 20")
+}
+else {
+        console.log("valor menor do que 20")
+}
+
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio5.js
@@ -0,0 +1,17 @@
+// Maior Valor
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maior = numbers [0];
+
+    for (index = 0; index < numbers.length; index +=1) {
+        if(numbers[index] > maior) {
+            maior = numbers[index];
+        }
+    }
+     
+    
+
+console.log(maior);
+
+
+
