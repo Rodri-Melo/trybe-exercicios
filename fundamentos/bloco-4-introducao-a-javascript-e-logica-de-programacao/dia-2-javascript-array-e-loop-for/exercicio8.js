61b2a7995370f19a33b3f336c3c74b059c4d0ca7
Rodrigo Neves <rnevesdemello@gmail.com>
2022-07-21 21:10:22 -0300

    tudo pronto

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio8.js
@@ -0,0 +1,12 @@
+let counter = 1;
+
+console.log(counter);
+
+while (counter < 25) {
+    counter += 1;
+    console.log(counter);
+}
+
+
+
+console.log(counter);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio9.js
@@ -0,0 +1,10 @@
+let counter = 1;
+
+console.log(counter/2);
+
+while (counter < 25) {
+    counter += 1;
+    console.log(counter/2);
+}
+
+console.log(counter/2);
