bb28999e3b2197658514bbcccf87595f0d33fa32
Rodrigo Neves <rnevesdemello@gmail.com>
2022-07-24 16:14:53 -0300

    exercicio 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio1.js
@@ -0,0 +1,11 @@
+let n = 10;
+
+
+for (let index = 1; index <= 10; index += 1) {
+
+    n *= index;
+
+
+
+}
+console.log(n);
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -0,0 +1,14 @@
+let word = 'tryber';
+let reverse = '';
+
+reverse = word.split('').reverse().join('');
+
+console.log(reverse)
+
+    split(tryber);
+    
+    reverse();
+    
+    join(tryber);
+
+console.log(tryber);
